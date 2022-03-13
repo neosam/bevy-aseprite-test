9aed7b896bfe2ec40ec7c52fd273f28225311dd6
Simon Goller <neosam+github@posteo.de>
2022-03-13 21:25:56 +0100

    Test clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -85,6 +85,7 @@ pub fn player_walk(
     mut query: Query<(&mut AsepriteAnimation, &mut Direction, &mut Velocity), With<Player>>,
     mut input_actions: EventReader<InputAction>,
 ) {
+    (0..10).filter(|x| *x == 0).next();
     let speed = 40.;
     if let Ok((mut player_animation, mut direction, mut velocity)) = query.get_single_mut() {
         for event in input_actions.iter() {
